d4a0e355992a61d08004633571162351508c19e7
pooria <purya1414@gmail.com>
2024-04-15 22:27:26 +0330

    RETHEME

--- pages/index.php
+++ pages/index.php
@@ -2,7 +2,6 @@
 session_start();
 if(!isset($_SESSION['Auth'])){
   header("location:./login.php");
-  // var_dump("ASS");die();
 }
 include "./../layout/head.php";
 include "./../DB/dataBase.php";
@@ -29,58 +28,80 @@ $data =  $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php 
 require "./../layout/header.php";
 ?>
-<div class="container mt-5" >
-  <!-- Add Area -->
-  <div class="w-25 bg-primary p-3 rounded">
-    <label class="mb-3 text-white" for="">Add new File</label>
-    <form action="./../act/sendData2DB.php" method="POST">
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="addon-wrapping">Name</span>
-        <input type="text" name="name" class="form-control" placeholder="Name" aria-label="Username" aria-describedby="addon-wrapping">
+<!-- XXX -->
+<div class="d-flex row">
+
+  <!-- MainArea -->
+  <div class="d-flex col-11">
+    <div class="mt-5 container" >
+    <!-- Add Area -->
+      <div class="w-25 bg-primary p-3 rounded">
+        <label class="mb-3 text-white" for="">Add new File</label>
+        <form action="./../act/sendData2DB.php" method="POST">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="addon-wrapping">Name</span>
+            <input type="text" name="name" class="form-control" placeholder="Name" aria-label="Username" aria-describedby="addon-wrapping">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="addon-wrapping">Day</span>
+            <input type="text" name="discharge_day" class="form-control" value="<?= convert_persian_to_english($dateOfJdate)?>" placeholder="discharge_day" aria-label="Username" aria-describedby="addon-wrapping">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="addon-wrapping">Mouth</span>
+            <select class="form-control" name="discharge_mouth" id="">
+            <option value="1" <?php if($f == "1"){echo "selected";}?>>فروردین</option>
+            <option value="2" <?php if($f == "2")echo "selected";?>>Ordibehesht</option>
+            <option value="3">Khordad</option>
+            <option value="4">Tir</option>
+            <option value="5">Mordad</option>
+            <option value="6">Shahrivar</option>
+            <option value="7">Mehr</option>
+            <option value="12" <?php if($f == "12"){echo "selected";}?>>Esfand</option>
+          </select>
+          </div>
+          <div class="d-flex justify-content-center">
+          <button type="submit" class="btn btn-dark">Add</button>
+          </div>
+        </form>
       </div>
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="addon-wrapping">Day</span>
-        <input type="text" name="discharge_day" class="form-control" value="<?= convert_persian_to_english($dateOfJdate)?>" placeholder="discharge_day" aria-label="Username" aria-describedby="addon-wrapping">
-      </div>
-      <div class="input-group mb-3">
-        <span class="input-group-text" id="addon-wrapping">Mouth</span>
-        <select class="form-control" name="discharge_mouth" id="">
-        <option value="1" <?php if($f == "1"){echo "selected";}?>>فروردین</option>
-        <option value="2" <?php if($f == "2")echo "selected";?>>Ordibehesht</option>
-        <option value="3">Khordad</option>
-        <option value="4">Tir</option>
-        <option value="5">Mordad</option>
-        <option value="6">Shahrivar</option>
-        <option value="7">Mehr</option>
-        <option value="12" <?php if($f == "12"){echo "selected";}?>>Esfand</option>
-      </select>
-      </div>
-      <div class="d-flex justify-content-center">
-      <button type="submit" class="btn btn-dark">Add</button>
-      </div>
-    </form>
-  </div>
-  <!-- Show Area -->
-  <div>
-    <div class="d-flex flex-wrap mt-3">
-    <?php  foreach($data as $dt){   ?>
-      <div class="mx-1 px-2 py-1 bg-primary my-1 d-flex flex-column justify-content-center align-items-center rounded">
-        <p class="fs-4"><?= $dt['name']?></p>
-        <div style="display: flex;">
-          <p class="fs-4">Discharge Date : </p>
-          <p class="fs-4"><?= $dt['discharge_day'];?></p>
-          <p class="fs-4"><?= witchMouth($dt['discharge_mouth']);?></p>
+    <!-- Show Area -->
+        <div>
+          <div class="d-flex flex-wrap mt-3">
+          <?php  foreach($data as $dt){   ?>
+            <div class="mx-1 px-2 py-1 bg-primary my-1 d-flex flex-column justify-content-center align-items-center rounded">
+              <p class="fs-4"><?= $dt['name']?></p>
+              <div style="display: flex;">
+                <p class="fs-4">Discharge Date : </p>
+                <p class="fs-4"><?= $dt['discharge_day'];?></p>
+                <p class="fs-4"><?= witchMouth($dt['discharge_mouth']);?></p>
+              </div>
+              <div class="d-flex justify-content-between">
+                <a class="btn btn-primary" href="./fileSingle.php?id=<?= $dt['id'];?>">Details</a>
+                <a class="btn btn-primary" href="./../act/delete.php?id=<?= $dt['id']; ?>">Delete</a>
+              </div>
+            </div>
+          
+          <?php }?>
         </div>
-        <div class="d-flex justify-content-between">
-          <a class="btn btn-primary" href="./fileSingle.php?id=<?= $dt['id'];?>">Details</a>
-          <a class="btn btn-primary" href="./../act/delete.php?id=<?= $dt['id']; ?>">Delete</a>
-        </div>
       </div>
-      
-      <?php }?>
     </div>
   </div>
+    <!-- SideBar -->
+    <div class="col-1 bg-dark" style="min-height: 100%;">
+      <ul class="pt-2 w-100">
+          <li class="nav-item">
+            <a class="nav-link text-white" aria-current="page" href="./index.php">Add New File</a>
+          </li>
+            <hr class="text-white">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="./allFiles.php">All Files</a>
+          </li>
+      </ul>
+  </div>
+
 </div>
+<!-- XXX -->
+
 
 </body>
 </html>
--- pages/indexOLD.php
+++ pages/indexOLD.php
@@ -1,5 +1,8 @@
 <?php 
 session_start();
+if(!isset($_SESSION['Auth'])){
+  header("location:./login.php");
+}
 include "./../layout/head.php";
 include "./../DB/dataBase.php";
 include "./../helpers/withchMF.php";
@@ -21,20 +24,26 @@ $stmt->execute();
 $data =  $stmt->fetchAll(PDO::FETCH_ASSOC);
 //END OF QUERY ;
 ?>
-<body dir="rtl">
+<body>
 <?php 
 require "./../layout/header.php";
 ?>
-<div style="display: flex;justify-content: space-between;">
-<div>
-    <h3 class="mb-6-25">افزودن پرونده جدید</h3>
-     <form action="./../act/sendData2DB.php" method="POST">
-      <label for="name">نام : </label>
-      <input name="name" type="text">
-      <label for="discharge_day">روز : </label>
-      <input name="discharge_day" type="text" value="<?= convert_persian_to_english($dateOfJdate); ?>">
-      <label for="discharge_mouth">ماه : </label>
-      <select name="discharge_mouth" id="">
+<div class="container mt-5" >
+  <!-- Add Area -->
+  <div class="w-25 bg-primary p-3 rounded">
+    <label class="mb-3 text-white" for="">Add new File</label>
+    <form action="./../act/sendData2DB.php" method="POST">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="addon-wrapping">Name</span>
+        <input type="text" name="name" class="form-control" placeholder="Name" aria-label="Username" aria-describedby="addon-wrapping">
+      </div>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="addon-wrapping">Day</span>
+        <input type="text" name="discharge_day" class="form-control" value="<?= convert_persian_to_english($dateOfJdate)?>" placeholder="discharge_day" aria-label="Username" aria-describedby="addon-wrapping">
+      </div>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="addon-wrapping">Mouth</span>
+        <select class="form-control" name="discharge_mouth" id="">
         <option value="1" <?php if($f == "1"){echo "selected";}?>>فروردین</option>
         <option value="2" <?php if($f == "2")echo "selected";?>>Ordibehesht</option>
         <option value="3">Khordad</option>
@@ -44,51 +53,33 @@ require "./../layout/header.php";
         <option value="7">Mehr</option>
         <option value="12" <?php if($f == "12"){echo "selected";}?>>Esfand</option>
       </select>
-      <button style="padding: 2px 3px;font-family:yekanReg" type="submit">افزودن</button>
-     </form>
-    </div>
-<!-- END NEW FILE ; -->
-
-    </div>
-    <div style="display: flex;justify-content: CENTER;flex-wrap: wrap;">
-      <?php  foreach($data as $dt){   ?>
-      <div style="    background-color:#585858;
-    margin-inline: 5px;
-    margin: 5px 6px;
-    min-width: 240px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 17px 5PX;
-    border-radius: 5px;
-    color:aliceblue;
-    ">
-        <h3><?= $dt['name']?></h3>
+      </div>
+      <div class="d-flex justify-content-center">
+      <button type="submit" class="btn btn-dark">Add</button>
+      </div>
+    </form>
+  </div>
+  <!-- Show Area -->
+  <div>
+    <div class="d-flex flex-wrap mt-3">
+    <?php  foreach($data as $dt){   ?>
+      <div class="mx-1 px-2 py-1 bg-primary my-1 d-flex flex-column justify-content-center align-items-center rounded">
+        <p class="fs-4"><?= $dt['name']?></p>
         <div style="display: flex;">
-          <h3>تاریخ ترخیص : </h3>
-          <h3><?= $dt['discharge_day'];?></h3>
-          <h3><?= witchMouth($dt['discharge_mouth']);?></h3>
+          <p class="fs-4">Discharge Date : </p>
+          <p class="fs-4"><?= $dt['discharge_day'];?></p>
+          <p class="fs-4"><?= witchMouth($dt['discharge_mouth']);?></p>
         </div>
-        
-       
-         
-             <div style="display: flex;justify-content: space-around;">
-               
-             
-             <button type="button" style="background: #ffbc00;
-             padding: 4px 27px;
-             border-radius: 5px;"><a style="text-decoration: none;color: #034c70;" href="./fileSingle.php?id=<?= $dt['id'];?>">جزئیات</a></button> 
-             <button style="background-color:#f50b0b ;padding: 4px 27px; border-radius: 5px;" type="button"><a style="text-decoration: none; color:whitesmoke" href="./../act/delete.php?id=<?= $dt['id']; ?>">حذف</a></button>
-            </div>
+        <div class="d-flex justify-content-between">
+          <a class="btn btn-primary" href="./fileSingle.php?id=<?= $dt['id'];?>">Details</a>
+          <a class="btn btn-primary" href="./../act/delete.php?id=<?= $dt['id']; ?>">Delete</a>
+        </div>
       </div>
       
       <?php }?>
     </div>
+  </div>
+</div>
 
-
-    <?php
-    include "./../layout/script.php";
-    ?>
 </body>
 </html>
